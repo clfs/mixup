4f6122f24e62ac4e59a913a668b48dad0d3976e0
Calvin Figuereo-Supraner <mail@calvin.page>
2024-06-24 20:56:53 -0700

    Add deterministic mode

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"math/rand/v2"
@@ -11,6 +12,16 @@ import (
 func main() {
 	log.SetPrefix("mixup: ")
 	log.SetFlags(0)
+
+	deterministicFlag := flag.Bool("d", false, "enable deterministic mode")
+	helpFlag := flag.Bool("h", false, "show help")
+
+	flag.Parse()
+
+	if *helpFlag {
+		flag.Usage()
+		return
+	}
 
 	var lines []string
 
@@ -22,9 +33,15 @@ func main() {
 		log.Fatal(err)
 	}
 
-	rand.Shuffle(len(lines), func(i, j int) {
+	fn := func(i, j int) {
 		lines[i], lines[j] = lines[j], lines[i]
-	})
+	}
+
+	if *deterministicFlag {
+		rand.New(rand.NewPCG(0, 0)).Shuffle(len(lines), fn)
+	} else {
+		rand.Shuffle(len(lines), fn)
+	}
 
 	for _, line := range lines {
 		fmt.Println(line)
